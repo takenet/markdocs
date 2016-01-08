0913bfa5c09285328f6a2605ffd438ce1fa006b0
Rafael Romão <rafaelm@takenet.com.br>
2016-01-08 15:59:32 -0200

    remove cache and log

--- src/DynamicNodeProvider.cs
+++ src/DynamicNodeProvider.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace Takenet.MarkDocs
 {
--- src/MarkDocsProvider.cs
+++ src/MarkDocsProvider.cs
@@ -1,84 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Reflection;
-using System.Runtime.Caching;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
-using NLog;
 
 namespace Takenet.MarkDocs
 {
     public class MarkDocsProvider
     {
-        private class MarkDocsCache
-        {
-            public MarkDocsCache(ObjectCache objectCache)
-            {
-                ObjectCache = objectCache;
-            }
-
-            private ObjectCache ObjectCache { get; }
-
-            internal bool TryGetCachedValue(string key, out object value, [CallerMemberName] string group = null)
-            {
-                key = $"{group}-{key}";
-                if (!ObjectCache.Contains(key))
-                {
-                    value = null;
-                    return false;
-                }
-                value = ObjectCache.Get(key);
-                return true;
-            }
-
-            internal void SetCachedValue(string key, object value, [CallerMemberName] string group = null)
-            {
-                key = $"{group}-{key}";
-                ObjectCache.Set(key, value, DateTimeOffset.Now.AddDays(1));
-            }
-        }
-
         private static readonly MarkDocsSection MarkDocsSettings = ConfigurationManager.GetSection("markdocs") as MarkDocsSection;
-
-        public MarkDocsProvider(ObjectCache cache)
-        {
-            Cache = new MarkDocsCache(cache);
-        }
-
-        private readonly Logger Log = LogManager.GetCurrentClassLogger();
-
-        private void WriteCurrentCultureInfoToLog([CallerMemberName] string methodName = null)
-        {
-            Log.Debug($"@{methodName}: CultureInfo.CurrentUICulture = {CultureInfo.CurrentUICulture}");
-        }
-
-        private static Assembly GetWebEntryAssembly()
-        {
-            if (Assembly.GetEntryAssembly() != null)
-                return Assembly.GetEntryAssembly();
-
-            if (System.Web.HttpContext.Current == null ||
-                System.Web.HttpContext.Current.ApplicationInstance == null)
-                return null;
-
-            var type = System.Web.HttpContext.Current.ApplicationInstance.GetType();
-            while (type != null && type.Namespace == "ASP")
-            {
-                type = type.BaseType;
-            }
-
-            return type?.Assembly;
-        }
-
-        private MarkDocsCache Cache { get; }
 
         public NodeElement Root => MarkDocsSettings.Items.Single();
 
@@ -97,12 +32,6 @@ namespace Takenet.MarkDocs
 
         private string BaseUrlForRawFiles(string nodeId)
         {
-            WriteCurrentCultureInfoToLog();
-
-            object value;
-            if (Cache.TryGetCachedValue(nodeId, out value))
-                return value as string;
-
             var allNodes = Flatten();
             var node = allNodes.SingleOrDefault(n => n.TargetFolder == nodeId);
             if (node == null)
@@ -111,20 +40,11 @@ namespace Takenet.MarkDocs
             var localizationPathPart = node.Localized ? $"{CultureInfo.CurrentUICulture.TwoLetterISOLanguageName}" : string.Empty;
             var result = $"https://raw.githubusercontent.com/{node.Owner}/{node.Repo}/{node.Branch}/{node.SourceFolder}/{localizationPathPart}";
 
-            Cache.SetCachedValue(nodeId, result);
-
             return result;
         }
 
         public async Task<string> GetDocumentAsync(string folder, string document)
         {
-            WriteCurrentCultureInfoToLog();
-
-            var key = $"{folder}.{document}";
-            object value;
-            if (Cache.TryGetCachedValue(key, out value))
-                return value as string;
-
             const string errorMessage = "Could not find the requested document!";
 
             var sourceUrl = BaseUrlForRawFiles(folder);
@@ -142,17 +62,12 @@ namespace Takenet.MarkDocs
             using (var webClient = new HttpClient())
             {
                 var result = await webClient.GetStringAsync(resourceUri);
-
-                Cache.SetCachedValue(key, result);
-
                 return result;
             }
         }
 
         private async Task<string> GetFileNameAsync(NodeElement node, string document)
         {
-            WriteCurrentCultureInfoToLog();
-
             var fileNames = await GetFileNamesAsync(node).ConfigureAwait(false); ;
             var fileName = fileNames.Single(fn => fn.EndsWith($"-{document}.md"));
             return fileName;
@@ -160,8 +75,6 @@ namespace Takenet.MarkDocs
 
         private async Task<IEnumerable<string>> GetFileNamesAsync(NodeElement node)
         {
-            WriteCurrentCultureInfoToLog();
-
             var urls = await GetUrlsFromChildItemsAsync(node).ConfigureAwait(false); ;
             var docs = urls.Single(u => u.Key == node.SourceFolder).Value;
             var cultureCode = string.Empty;
@@ -172,34 +85,19 @@ namespace Takenet.MarkDocs
                 docs = urls.Single(u => u.Key == cultureCode).Value;
             }
 
-            var key = $"{node.TargetFolder}-{cultureCode}";
-            object value;
-            if (Cache.TryGetCachedValue(key, out value))
-                return value as IEnumerable<string>;
-
             var fileNames = await GetChildItemsFileNamesAsync(docs, node.Username, node.Password);
-
-            Cache.SetCachedValue(key, fileNames);
 
             return fileNames;
         }
 
         internal async Task<IDictionary<string, string>> GetUrlsFromChildItemsAsync(NodeElement item)
         {
-            WriteCurrentCultureInfoToLog();
-
             var parentUrl = $"https://api.github.com/repos/{item.Owner}/{item.Repo}/git/trees/{item.Branch}";
             return await GetUrlsFromChildItemsAsync(parentUrl, item.Username, item.Password).ConfigureAwait(false);
         }
 
         internal async Task<IDictionary<string, string>> GetUrlsFromChildItemsAsync(string parentUrl, string username, string password)
         {
-            WriteCurrentCultureInfoToLog();
-
-            object value;
-            if (Cache.TryGetCachedValue(parentUrl, out value))
-                return value as IDictionary<string, string>;
-
             var urls = new Dictionary<string, string>();
             var data = await GetStringAsync(parentUrl, username, password).ConfigureAwait(false);
             var json = JObject.Parse(data);
@@ -208,39 +106,21 @@ namespace Takenet.MarkDocs
             {
                 urls[url["path"].Value<string>()] = url["url"].Value<string>();
             }
-
-            Cache.SetCachedValue(parentUrl, urls);
-
             return urls;
         }
 
         internal async Task<IEnumerable<string>> GetChildItemsFileNamesAsync(string parentUrl, string username, string password)
         {
-            WriteCurrentCultureInfoToLog();
-
-            object value;
-            if (Cache.TryGetCachedValue(parentUrl, out value))
-                return value as IEnumerable<string>;
-
             var result = new List<string>();
             var data = await GetStringAsync(parentUrl, username, password).ConfigureAwait(false);
             var json = JObject.Parse(data);
             var tree = json["tree"] as JArray;
             result.AddRange(tree.Select(item => item["path"].Value<string>()));
-
-            Cache.SetCachedValue(parentUrl, result);
-
             return result;
         }
 
         private async Task<string> GetStringAsync(string url, string username, string password)
         {
-            WriteCurrentCultureInfoToLog();
-
-            object value;
-            if (Cache.TryGetCachedValue(url, out value))
-                return value as string;
-
             using (var webClient = new HttpClient())
             {
                 var uri = new Uri(url, UriKind.Absolute);
@@ -252,8 +132,6 @@ namespace Takenet.MarkDocs
                 var response = await webClient.SendAsync(request).ConfigureAwait(false);
 
                 var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                Cache.SetCachedValue(url, result);
 
                 return result;
             }
